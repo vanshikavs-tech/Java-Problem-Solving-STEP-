724d0495103c76c37b51556d02a171aea19e4bfa
Vanshika Singh <vanshikanr06@gmail.com>
2026-08-13 22:06:53 +0530

    Keep only README on main

---
+++ Week-2/Product_Inventory_CSV_Parser.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Product_Inventory_CSV_Parser {
+
+    static void parseInventoryRecord(String csvLine) {
+
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+            System.out.println("Invalid Record");
+        } else {
+            System.out.println(
+                "Product: " + fields[0]
+                + " | SKU: " + fields[1]
+                + " | Qty: " + fields[2]
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter inventory record: ");
+        String csvLine = sc.nextLine();
+
+        parseInventoryRecord(csvLine);
+
+        sc.close();
+    }
+}
---
+++ Week-2/Word_Reversal_Encoder.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Word_Reversal_Encoder {
+
+    static String reverseEachWord(String sentence) {
+
+        String[] words = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+
+            StringBuilder word = new StringBuilder(words[i]);
+            word.reverse();
+
+            result.append(word);
+
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter sentence: ");
+        String sentence = sc.nextLine();
+
+        String result = reverseEachWord(sentence);
+
+        System.out.println("Reversed sentence: " + result);
+
+        sc.close();
+    }
+}
