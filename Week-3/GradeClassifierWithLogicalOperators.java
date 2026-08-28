01d1f074b90173a809aa25b6f479d8cd16528ccd
Vanshika Singh <vanshikanr06@gmail.com>
2026-08-29 00:07:27 +0530

    Add Level 2 and Level 3 Java problems

---
+++ Week-3/GradeClassifierWithLogicalOperators.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class GradeClassifierWithLogicalOperators {
+
+    public static void classifyWithAttendance(int marks, int attendance) {
+
+        if (attendance >= 75 && marks >= 40) {
+
+            if (marks >= 90) {
+                System.out.println("Grade: A");
+            } else if (marks >= 75) {
+                System.out.println("Grade: B");
+            } else if (marks >= 60) {
+                System.out.println("Grade: C");
+            } else {
+                System.out.println("Grade: D");
+            }
+
+        } else {
+            System.out.println("Detained");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int marks = sc.nextInt();
+        int attendance = sc.nextInt();
+
+        classifyWithAttendance(marks, attendance);
+
+        sc.close();
+    }
+}
