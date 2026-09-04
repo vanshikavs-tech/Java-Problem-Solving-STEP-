91e9f1d4dd2db3939a8e5bfaef6c00b6056df049
Vanshika Singh <vanshikanr06@gmail.com>
2026-09-05 01:16:34 +0530

    Week-2 branch

---
+++ Week-2/ASSIGNMENT/ATM_PIN_Length_Validator.java
@@ -0,0 +1,25 @@
+package ASSIGNMENT;
+import java.util.Scanner;
+
+public class ATM_PIN_Length_Validator {
+
+    static void checkPinLength(String pin) {
+        if (pin.length() != 4) {
+            System.out.println("Invalid PIN — must be exactly 4 digits.");
+        } else {
+            System.out.println("PIN length OK.");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter PIN: ");
+        String pin = sc.nextLine();
+
+        checkPinLength(pin);
+
+        sc.close();
+    }
+}
