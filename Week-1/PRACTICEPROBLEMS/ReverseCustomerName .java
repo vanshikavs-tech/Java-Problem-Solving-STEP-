bd0d86c62329f7131662b77060ee4e90b5c199bb
Vanshika Singh <vanshikanr06@gmail.com>
2026-09-12 17:32:02 +0530

    Add complete Week 1 Java programs

---
+++ Week-1/PRACTICEPROBLEMS/ReverseCustomerName .java
@@ -0,0 +1,40 @@
+package PRACTICEPROBLEMS;
+
+import java.util.*;
+
+public class ReverseCustomerName {
+
+    static String reverseCustomerName(String customerName) {
+
+        char[] characters = customerName.toCharArray();
+
+        int left = 0;
+        int right = characters.length - 1;
+
+        while (left < right) {
+            char temp = characters[left];
+            characters[left] = characters[right];
+            characters[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(characters);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter customer name: ");
+        String customerName = sc.nextLine();
+
+        String reversedName = reverseCustomerName(customerName);
+
+        System.out.println("Original Name: " + customerName);
+        System.out.println("Reversed Name: " + reversedName);
+
+        sc.close();
+    }
+}
